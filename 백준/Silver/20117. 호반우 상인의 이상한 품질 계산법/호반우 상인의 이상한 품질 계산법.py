f5c3f55c96622fad057a5623ed1818949219528f
구자건 <147472859+AruJoy@users.noreply.github.com>
2024-07-17 11:33:32 +0900

    [Silver I] Title: 호반우 상인의 이상한 품질 계산법, Time: 228 ms, Memory: 42444 KB -BaekjoonHub

--- 백준/Silver/20117. 호반우 상인의 이상한 품질 계산법/호반우 상인의 이상한 품질 계산법.py
+++ 백준/Silver/20117. 호반우 상인의 이상한 품질 계산법/호반우 상인의 이상한 품질 계산법.py
@@ -5,12 +5,13 @@ input = stdin.readline
 def ho_ban_woo(n_cow):
     cow_list = list(map(int, input().split(' ')))
     cow_list.sort()
+    cow_list = deque(cow_list)
 
     count = 0
     while cow_list:
         count += cow_list[len(cow_list)//2]
-        cow_list.pop(0)
-        
+        cow_list.popleft()
+
     return count
 
 n_cow = int(input())
