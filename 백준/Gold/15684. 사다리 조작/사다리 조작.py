f32a35dbd378345b4291c4e15a239a35c6208926
아루조이 <147472859+AruJoy@users.noreply.github.com>
2025-09-04 17:01:11 +0900

    [Gold III] Title: 사다리 조작, Time: 1840 ms, Memory: 111932 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.py
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.py
@@ -12,30 +12,22 @@ def get_input():
     return vertical_lines, horizontal_lines, ladder_map
 
 def judge_answer(vertical_lines:int, horizontal_lines:int, ladder_map:list):
-    dx = [-1, 0]
     for current_x in range(vertical_lines):
         position = current_x
         for y in range(horizontal_lines):
-            for i in range(2):
-                x = position + dx[i]
-                if (x < 0 or vertical_lines - 2 < x):
-                    continue
-                if ladder_map[y][x] :
-                    if i == 0:
-                        position -=1
-                    else:
-                        position += 1
-                    break
+            if position < vertical_lines - 1 and ladder_map[y][position]:
+                position += 1
+            elif position > 0 and ladder_map[y][position - 1]:
+                position -= 1
         if position != current_x:
             return False
     return True
-def put_ladder(vertical_lines:int, horizontal_lines:int,ladder_map:list, depth: int, start_y:int, start_x:int ):
-    global min_ans
+def put_ladder(vertical_lines:int, horizontal_lines:int,ladder_map:list, depth: int, min_ans:list, start_y:int, start_x:int ):
     answer = 4
     dx = [-1, 1]
     if judge_answer(vertical_lines, horizontal_lines, ladder_map):
         return depth
-    elif depth == 3 or depth >= min_ans - 1: return 4
+    elif depth >= 3 or depth >= min_ans[0] - 1: return 4
     
     for y in range(start_y, horizontal_lines):
         for current_x in range(start_x if y == start_y else 0, vertical_lines-1):
@@ -51,15 +43,15 @@ def put_ladder(vertical_lines:int, horizontal_lines:int,ladder_map:list, depth: 
                     break
             if result:
                 ladder_map[y][current_x] = True
-                answer = min(answer, put_ladder(vertical_lines, horizontal_lines, ladder_map, depth+1, y, current_x))
-                min_ans = answer
+                answer = min(answer, put_ladder(vertical_lines, horizontal_lines, ladder_map, depth+1, min_ans, y, current_x))
+                min_ans[0] = answer
                 ladder_map[y][current_x] = False
                 if answer == depth:
                     return answer
     return answer
 def get_answer(vertical_lines:int, horizontal_lines:int, ladder_map:list):
-    answer = put_ladder(vertical_lines, horizontal_lines, ladder_map, 0, 0, 0)
+    min_ans = [4]
+    answer = put_ladder(vertical_lines, horizontal_lines, ladder_map, 0, min_ans, 0, 0)
     return answer if answer < 4 else -1
 vertical_lines, horizontal_lines, ladder_map = get_input()
-min_ans = 4
 print(get_answer(vertical_lines, horizontal_lines, ladder_map))
