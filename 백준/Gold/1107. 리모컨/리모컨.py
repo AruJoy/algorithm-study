0e10fe788e48113688377d5618369cf448c97da3
아루조이 <147472859+AruJoy@users.noreply.github.com>
2025-09-18 02:10:54 +0900

    [Gold IV] Title: 리모컨, Time: 3260 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -0,0 +1,33 @@
+from sys import stdin
+input = stdin.readline
+
+def get_input():
+    target = int(input().strip())
+    broken_count = int(input().strip()) 
+    key_list = [i for i in range(10)]
+    if broken_count>0:
+        broken_key_list = list(map(int, input().split(" ")))
+        broken_key_list.sort(reverse=True)
+        for key in broken_key_list:
+            key_list.pop(key)
+    numbers = 1 if target == 0 else len(str(abs(target)))
+    if target // 10 ** (numbers-1) == 1 and 10 ** (numbers) - target < 10:
+        return target, key_list, 10-broken_count, numbers+1
+    return target, key_list, 10-broken_count, numbers
+
+def get_push_count(target, key_list, key_count, numbers ,current_value ,depth):
+    now = 100
+    target/10
+    value = current_value
+    answer = abs(target - now)
+    for i in range(key_count):
+        add_value = key_list[i] * (10**depth)
+        value += add_value
+        answer = min(abs(value - target) + depth+1, answer)
+        if depth < numbers:
+            answer = min(answer, get_push_count(target, key_list, key_count, numbers, value , depth+1))
+        value -= add_value
+    return answer
+
+target, key_list, key_count, numbers = get_input()
+print(get_push_count(target, key_list, key_count, numbers, 0, 0))
