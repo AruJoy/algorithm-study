f14b5cff823c31404d8d424c1a0de9be6dad6df5
아루조이 <147472859+AruJoy@users.noreply.github.com>
2025-09-18 22:03:39 +0900

    [Gold III] Title: Guess, Time: 2840 ms, Memory: 119924 KB -BaekjoonHub

--- 백준/Gold/1248. Guess/Guess.py
+++ 백준/Gold/1248. Guess/Guess.py
@@ -7,58 +7,54 @@ def get_input():
     sign_count = (scale**2 - scale)//2 +scale
     sign_list = list(input().strip())
     y = 0
-    x = scale
-    z = scale
+    x = 0
+    z = 0
     for i in range(sign_count):
-        matrix[y][scale-x] = sign_list[i]
-        if x == 1:
-            z -= 1
+        matrix[y][x] = sign_list[i]
+        if x == scale-1:
+            z += 1
             x = z
             y += 1
             continue
-        x -= 1
-    return scale, matrix, sign_count
-def check_number_list(matrix, number_list, depth):
-    check_count = (depth**2 - depth)//2 +depth
-    x = 0
-    y = 0
-    j = 0
-    num_sum = 0
-    for i in range(check_count):
-        num_sum += number_list[x]
-        if num_sum > 0 and matrix[y][x] != "+":
-            return False
-        if num_sum < 0 and matrix[y][x] != "-":
-            return False
-        if num_sum == 0 and matrix[y][x] != "0":
-            return False
-        if x == depth-1:
-            j += 1
-            x = j
-            y += 1
-            num_sum = 0
-            continue
         x += 1
+    return scale, matrix, sign_count
+def check_number_list(matrix, number_list, dp, depth):
+    for i in range(depth):
+        num_sum = dp[i] + number_list[depth-1]
+        if num_sum > 0 and matrix[i][depth-1] != "+":
+            return False
+        if num_sum < 0 and matrix[i][depth-1] != "-":
+            return False
+        if num_sum == 0 and matrix[i][depth-1] != "0":
+            return False
+        dp[i] = num_sum
     return True
-def find_number_list(scale, matrix, sign_count, number_list, depth):
+def find_number_list(scale, matrix, sign_count, number_list, dp, depth):
     for i in range(0, 11):
         number_list.append(i)
-        if check_number_list(matrix, number_list, depth):
+        back_up_list = [j for j in dp]
+        if check_number_list(matrix, number_list, dp, depth):
             if depth == scale: return True
-            if find_number_list(scale, matrix, sign_count, number_list, depth+1):
+            if find_number_list(scale, matrix, sign_count, number_list, dp, depth+1):
                 return True
+        for j in range(scale):
+            dp[j] = back_up_list[j]
         number_list.pop()
         if i > 0:
             number_list.append(-i)
-            if check_number_list(matrix, number_list, depth):
+            back_up_list = [j for j in dp]
+            if check_number_list(matrix, number_list, dp, depth):
                 if depth == scale: return True
-                if find_number_list(scale, matrix, sign_count, number_list, depth+1):
+                if find_number_list(scale, matrix, sign_count, number_list, dp, depth+1):
                     return True
+            for j in range(scale):
+                dp[j] = back_up_list[j]
             number_list.pop()
     return False
 def get_solution(scale, matrix, sign_count):
     number_list = []
-    find_number_list(scale, matrix, sign_count, number_list, 1)
+    dp = [0 for i in range(scale)]
+    find_number_list(scale, matrix, sign_count, number_list, dp, 1)
     print(*number_list)
     return
 scale, matrix, sign_count = get_input()
