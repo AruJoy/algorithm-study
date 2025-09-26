f50c4e8544d675e0fe06fdba2a2a4211df17b966
아루조이 <147472859+AruJoy@users.noreply.github.com>
2025-09-26 17:05:22 +0900

    [Gold IV] Title: 부분합, Time: 120 ms, Memory: 45756 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -0,0 +1,33 @@
+from sys import stdin
+from collections import deque
+input = stdin.readline
+INF = float("INF")
+
+def main():
+    length, lo = map(int, input().split())
+    numbers = list(map(int, input().split()))
+    sum_list = [0 for _ in range(length)]
+    sum_list[0] = numbers[0]
+    sum_list2 = [0 for _ in range(length)]
+    sum_list2[0] = numbers[-1]
+    for i in range(1,length):
+        sum_list[i] = sum_list[i-1]+numbers[i]
+        sum_list2[i] = sum_list2[i-1] +numbers[-i-1] 
+    if sum_list[0] >= lo or sum_list2[0] >= lo:
+        print(1)
+        return
+    if sum_list[-1] < lo:
+        print(0)
+        return
+    answer = INF
+    i = 1
+    j = 0
+    while i < length:
+        while sum_list[i] - sum_list[j] >= lo or sum_list2[i] - sum_list2[i] >= lo:
+            answer = min(i - j, answer)
+            j += 1
+        i += 1
+    print(length if answer == INF else answer)
+    return
+if __name__ == "__main__":
+    main()
