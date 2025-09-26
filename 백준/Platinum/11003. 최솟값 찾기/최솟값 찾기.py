1a981bfdd3a9e3a94edce60df84974c68c7b6b9f
아루조이 <147472859+AruJoy@users.noreply.github.com>
2025-09-26 17:57:24 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 5136 ms, Memory: 872852 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
@@ -7,28 +7,15 @@ INF = float("INF")
 def main():
     numbers, length = map(int, input().split())
     number_list = list(map(int, input().split()))
-    heap = []
     que = deque()
-    remove_list = dict()
     answer = []
-    for i in range(length):
-        heappush(heap, number_list[i])
-        que.append(number_list[i])
-        answer.append(heap[0])
-    for i in range(length, numbers):
-        pop_number = que.popleft()
-        if pop_number in remove_list.keys():
-            remove_list[pop_number] += 1
-        else:
-            remove_list[pop_number] = 1
-        while heap and heap[0] in remove_list.keys():
-            remove_list[heap[0]] -= 1
-            if remove_list[heap[0]] == 0:
-                remove_list.pop(heap[0])
-            heappop(heap)
-        heappush(heap, number_list[i])
-        que.append(number_list[i])
-        answer.append(heap[0])
+    for i in range(numbers):
+        while que and que[-1][0] > number_list[i]:
+            que.pop()
+        que.append((number_list[i], i))
+        if que[0][1] <= i - length:
+            que.popleft()
+        answer.append(que[0][0])
     print(*answer)
     return
 if __name__ == "__main__":
