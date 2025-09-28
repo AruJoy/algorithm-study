96220666beaa4284a7ff185261760cda101b2f94
아루조이 <147472859+AruJoy@users.noreply.github.com>
2025-09-29 03:11:16 +0900

    [Gold V] Title: 용액, Time: 80 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,28 +1,29 @@
 from sys import stdin
 input = stdin.readline
-from collections import deque
-INF = float("INF")
 
 def main():
-    length = int(input().strip())
-    number_list = list(map(int, input().split()))
-    a, b = -1, -1
-    i = 0
-    j = length-1
-    answer = abs(number_list[j] + number_list[i])
-    a, b = number_list[i], number_list[j]
+    n = int(input().strip())
+    arr = list(map(int, input().split()))
+
+    i, j = 0, n - 1
+    best_sum = abs(arr[i] + arr[j])
+    a, b = arr[i], arr[j]
+
     while i < j:
-        cur_diff = abs(number_list[j] + number_list[i])
-        nxt_diff_1 = abs(number_list[j-1] + number_list[i])
-        nxt_diff_2 = abs(number_list[j] + number_list[i+1])
-        if cur_diff < answer:
-            a, b = number_list[i], number_list[j]
-            answer = cur_diff
-        if nxt_diff_1 > nxt_diff_2:
+        cur_sum = arr[i] + arr[j]
+        if abs(cur_sum) < best_sum:
+            best_sum = abs(cur_sum)
+            a, b = arr[i], arr[j]
+
+        if cur_sum > 0:
+            j -= 1
+
+        elif cur_sum < 0:
             i += 1
-            continue
-        j -= 1
+        else:
+            break
+
     print(a, b)
-    return
+
 if __name__ == "__main__":
     main()
