09cf4e7bb80874fd85248f3011e5a1f031c4049d
아루조이 <147472859+AruJoy@users.noreply.github.com>
2025-10-02 02:05:39 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 424 ms, Memory: 66624 KB, Score: 250 point -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -6,11 +6,11 @@ def main():
     num_list = list(map(int, input().split()))
     num_list.sort()
     ans = 0
+    pow_list = [1 for _ in range(n)]
+    for i in range(1,n):
+        pow_list[i] = (pow_list[i-1]*2)% MOD
     for i in range(n-1, -1, -1):
-        if i != 0:
-            ans += ((2**i)-1)*num_list[i]
-        if i+1 != n:
-            ans -= ((2**(n-i-1))-1)*num_list[i]
+        ans += num_list[i] * (((pow_list[i]-1) - pow_list[-i-1]+1))
         ans = ans % MOD
     print(ans)
     return
