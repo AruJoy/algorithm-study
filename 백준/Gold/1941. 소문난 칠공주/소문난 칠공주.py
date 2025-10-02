ed4586c2db5bfcf73bce9680a2a9646a34a4805d
아루조이 <147472859+AruJoy@users.noreply.github.com>
2025-10-02 19:38:05 +0900

    [Gold III] Title: 소문난 칠공주, Time: 2352 ms, Memory: 35016 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
@@ -24,28 +24,22 @@ def is_adjacent(mask):
                 que.append((y,x))
     return len(coord_set) == 0
 
-def union(class_room, found_list, bit, mask, count, y_count):
+def union(class_room, bit, mask, count, y_count):
     ans = 0
     if count == 7:
-        if found_list[mask]:
-            return 0
-        found_list[mask] = True
         return 1 if is_adjacent(mask) else 0
     for i in range(bit, 25):
         y, x = i//5, i%5
         nxt_count = y_count + (1 if class_room[y][x] == "Y" else 0)
         if nxt_count >= 4:
             continue
-        ans += union(class_room, found_list, i+1, mask | 1<<i, count + 1, nxt_count)
+        ans += union(class_room, i+1, mask | 1<<i, count + 1, nxt_count)
     return ans
 def main():
     class_room = list()
-    ans = 0
-    found_list = [False for i in range((1<<25)-1)]
     for _ in range(5):
         class_room.append(list(input().strip()))
-    print(union(class_room, found_list, 0, 0, 0, 0))
+    print(union(class_room, 0, 0, 0, 0))
     return
 if __name__ == "__main__":
     main()
-    
