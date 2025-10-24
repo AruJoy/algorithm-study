78e5a53bef0af193c9d4fea226f594529239df7a
아루조이 <147472859+AruJoy@users.noreply.github.com>
2025-10-24 22:21:34 +0900

    [Gold II] Title: 계보 복원가 호석, Time: 484 ms, Memory: 60584 KB, Score: 10 point -BaekjoonHub

---
+++ 백준/Gold/21276. 계보 복원가 호석/계보 복원가 호석.py
@@ -0,0 +1,63 @@
+from sys import stdin
+from collections import deque
+input = stdin.readline
+
+
+def find(union, cur):
+    if cur == union[cur]:
+        return cur
+    union[cur] = find(union, union[cur])
+    return union[cur]
+
+
+def get_answer(names, degree_map):
+    answer_map = dict()
+    ancients = list()
+    for name in names:
+        if degree_map[name][0] == 0:
+            ancients.append(name)
+            que = deque()
+            que.append(name)
+            while que:
+                cur = que.popleft()
+                answer_map[cur] = list()
+                degree_map[cur][0] -= 1
+                for nxt in degree_map[cur][1]:
+                    degree_map[nxt][0] -= 1
+                    if degree_map[nxt][0] == 0:
+                        answer_map[cur].append(nxt)
+                        if len(degree_map[nxt][1]) == 0:
+                            degree_map[nxt][0] -= 1
+                            continue
+                        que.append(nxt)
+    print(len(ancients))
+    print(*ancients)
+    for name in names:
+        if name in answer_map:
+            print(name +
+                  " " +
+                  str(len(answer_map[name])) + " " +
+                  " ".join(answer_map[name]))
+            continue
+        print(name + " 0")
+    return
+
+
+def main():
+    n_name = int(input().strip())
+    names = list(input().split())
+    names.sort()
+    n_count = int(input().strip())
+    degree_map = dict()
+    for name in names:
+        degree_map[name] = [0, list()]
+    for _ in range(n_count):
+        child, parent = input().split()
+        degree_map[child][0] += 1
+        degree_map[parent][1].append(child)
+    get_answer(names, degree_map)
+    return
+
+
+if __name__ == "__main__":
+    main()
