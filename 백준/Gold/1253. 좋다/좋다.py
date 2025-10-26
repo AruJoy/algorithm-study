c04a59af460573977bf3bd0bf9188c953c5c5675
아루조이 <147472859+AruJoy@users.noreply.github.com>
2025-10-26 22:54:18 +0900

    [Gold IV] Title: 좋다, Time: 5932 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -1,43 +1,39 @@
+from collections import deque
 from sys import stdin
+input = stdin.readline
 
-def is_num_list_good(num_list, list_size):
-    count = 0
-    for lp in range(list_size):
-        for rp in range(lp+1, list_size):
-            target = num_list[lp] + num_list[rp]
-            find_left_pointer = 0
-            find_right_pointer = list_size-1
-            while(find_left_pointer<=find_right_pointer):
-                find_pointer = (find_left_pointer + find_right_pointer)//2
-                if target < num_list[find_pointer]:
-                    find_right_pointer = find_pointer -1
-                    continue
-                if num_list[find_pointer] < target:
-                    find_left_pointer = find_pointer +1
-                    continue
-                find_left_pointer = find_pointer-1
-                find_right_pointer = find_pointer+1
-                if find_pointer != lp and find_pointer !=rp:
-                    if not is_counted[find_pointer]:
-                        count += 1
-                        is_counted[find_pointer] = True
-                while(0 <= find_left_pointer and num_list[find_left_pointer] == target):
-                    if find_left_pointer != lp and find_left_pointer !=rp:
-                        if not is_counted[find_left_pointer]:
-                            count += 1
-                            is_counted[find_left_pointer] = True
-                    find_left_pointer-=1
-                while(find_right_pointer<list_size and num_list[find_right_pointer] == target):
-                    if find_right_pointer != lp and find_right_pointer !=rp:
-                        if not is_counted[find_right_pointer]:
-                            count += 1
-                            is_counted[find_right_pointer] = True
-                    find_right_pointer+=1
-                break
-    return count
 
-list_size = int(stdin.readline())
-num_list = list(map(int, stdin.readline().split(' ')))
-is_counted = [False for _ in range(list_size)]
-num_list.sort()
-print(is_num_list_good(num_list, list_size))
+def test(cond_list, number):
+    last_index = len(cond_list) - 1
+    for i in range(last_index):
+        lo = i + 1
+        hi = last_index
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            result = cond_list[i] + cond_list[mid]
+            if result == number:
+                return True
+            if result < number:
+                lo = mid + 1
+                continue
+            hi = mid - 1
+    return False
+
+
+def main():
+    n_number = int(input().strip())
+    if n_number < 3:
+        print(0)
+        return
+    ans = 0
+    numbers = list(map(int, input().split()))
+    numbers.sort()
+    for i in range(n_number):
+        if (test(numbers[:i] + numbers[i + 1:], numbers[i])):
+            ans += 1
+    print(ans)
+    return
+
+
+if __name__ == "__main__":
+    main()
